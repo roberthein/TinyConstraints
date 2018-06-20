964d2317d423edb35f44d1a97a7d87f8a50ce243
marijnschilling <marijnschilling@gmail.com>
2018-06-20 22:22:50 +0200

    Explore the Constraint properties

--- Readme.playground/Contents.swift
+++ Readme.playground/Contents.swift
@@ -14,18 +14,39 @@ constraintView.backgroundColor = .red
 
 viewController.view.addSubview(constraintView)
 
-constraintView.centerX(to: viewController.view)
-constraintView.height(200)
-constraintView.width(200)
+let ding = constraintView.height(200)
+let attribute1 = ding.firstAttribute.rawValue
+let object1 = ding.firstItem
+let anchor1 = ding.firstAnchor
+let attribute2 = ding.secondAttribute.rawValue
+let object2 = ding.secondItem
+let anchor2 = ding.secondAnchor
+
 
 PlaygroundPage.current.liveView = viewController
 
-extension NSLayoutConstraint: CustomPlaygroundDisplayConvertible {
+extension Constraint: CustomPlaygroundDisplayConvertible {
     public var playgroundDescription: Any {
-        let test = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
-        test.backgroundColor = .red
 
-        return test
+        guard secondItem != nil else {
+            return onlyOneAttributeView
+        }
+
+        let superView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        superView.backgroundColor = .red
+
+
+        return "lalala"
+    }
+
+    var onlyOneAttributeView: Any {
+        let superView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        superView.backgroundColor = .red
+        superView.layer.cornerRadius = 20
+
+
+
+        return superView
     }
 }
 
