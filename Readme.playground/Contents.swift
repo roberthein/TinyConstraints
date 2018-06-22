48bee13bc523c5a3b97f2f69fd63721e7acc7d55
marijnschilling <marijnschilling@gmail.com>
2018-06-22 19:35:04 +0200

    Use the super awesome real images

--- Readme.playground/Contents.swift
+++ Readme.playground/Contents.swift
@@ -1,22 +1,43 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 import PlaygroundSupport
 import TinyConstraints
 
-var str = "Hello, playground"
+//: tl;dr Nothing but sugar.
+//: TinyConstraints is the syntactic sugar that makes Auto Layout sweeter for human use.
 
-//: Create your viewController
+// Bit annoying I need to write all this boilerplate code in my readme.
+// maybe i can create a convenience method and have a new viewController for every example.
+// or different playground pages
 let viewController = UIViewController()
-
 let constraintView = UIView()
 constraintView.backgroundColor = .red
-
 viewController.view.addSubview(constraintView)
 
-let ding = constraintView.height(200)
+//: Size
+//: Setting width and height with NSLayoutConstraint:
+
+constraintView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+constraintView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+
+//: with TinyConstraints:
+
+constraintView.height(100)
+constraintView.width(100)
 
 PlaygroundPage.current.liveView = viewController
+
+//: Edges
+//: Attaching a view to its superview with NSLayoutConstraint:
+
+//: with TinyConstraints:
+
+//: Center
+//: Constraining the center of a view to its superview with NSLayoutConstraint:
+
+//: with TinyConstraints:
+
+
+
 
 extension Constraint: CustomPlaygroundDisplayConvertible {
     public var playgroundDescription: Any {
@@ -33,25 +54,21 @@ extension Constraint: CustomPlaygroundDisplayConvertible {
     }
 
     var onlyOneAttributeView: Any {
-        var superView = UIView()
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 203, height: 203))
+        let imageView = UIImageView(image: UIImage(named: "height&width.png")!)
+
+        let label = UILabel(frame: CGRect(x: 0, y: 90, width: 200, height: 20))
+        label.textColor = .white
+        label.textAlignment = .center
+        label.text = "\(constant)"
+
+        view.addSubview(imageView)
+        imageView.addSubview(label)
 
         if firstAttribute == .height {
-
-            let label = UILabel(frame: CGRect(x: -50, y: 90, width: constant, height: 20))
-            label.textColor = .white
-            label.textAlignment = .center
-            superView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: constant))
-
-            superView.addSubview(label)
-            label.text = "<------\(constant)------>"
-            label.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1.5))
+            imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1.5))
         }
 
-        superView.backgroundColor = .red
-        superView.layer.cornerRadius = 20
-
-        return superView
+        return view
     }
 }
-
-
